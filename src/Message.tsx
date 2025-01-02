c66b41b6983b4ea6e2a6b4f8c74e33f8a0f0719f
BDhanush489 <bdhanush489@gmail.com>
2025-01-02 21:14:39 +0530

    1s

---
+++ src/Message.tsx
@@ -0,0 +1,18 @@
+//Pascal Casing Capitalise the first letter of every word
+
+function Message(){
+    //JSX code: JavaScript XML
+    return <h1>Hello World</h1>
+}
+
+function HelloMessage(){
+    const name="";
+    //JSX code: JavaScript XML
+    if(name){
+        return <h1>Hello {name}</h1>
+    }
+    return <h1>Hello World</h1>
+    
+}
+
+export default  HelloMessage;
